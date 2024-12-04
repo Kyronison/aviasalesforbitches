7600504fc4ec782330ac4a335da3d862618e20ae
Dana <florakdd@gmail.com>
2024-12-05 01:02:09 +0300

    password criteria have been simplified

--- app/schemas.py
+++ app/schemas.py
@@ -48,10 +48,6 @@ class UserCreate(UserBase):
     def validate_password(cls, value: str) -> str:
         if len(value) < 8:
             raise ValueError("Пароль должен быть длиной минимум 8 символов")
-        if not any(char.isdigit() for char in value):
-            raise ValueError("Пароль должен содержать хотя бы одну цифру")
-        if not any(char.isupper() for char in value):
-            raise ValueError("Пароль должен содержать хотя бы одну заглавную букву")
         if not re.fullmatch(r"[a-zA-Z0-9]+", value):
             raise ValueError("Пароль может содержать только английские буквы и цифры")
         return value
