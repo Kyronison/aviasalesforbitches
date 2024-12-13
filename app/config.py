dca3447bf98f7ab86af5a14a8093c9480d35e5ae
Dana <florakdd@gmail.com>
2024-12-13 15:09:52 +0300

    config.py

--- app/config.py
+++ app/config.py
@@ -8,4 +8,3 @@ load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@localhost/dbname')
 AVIASALES_API_KEY = os.getenv('AVIASALES_API_KEY', 'your_aviasales_api_key')
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN', 'your_telegram_bot_token')
-
