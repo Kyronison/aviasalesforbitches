4c86749cb13d43269baaa50c11a5b2e656e8329d
TimofeiSmirnov <ts5310m@gmail.com>
2024-12-15 22:14:19 +0300

    Add bot functions to send messages and add gitignor with config and .env

--- app/telegram_bot/handlers.py
+++ app/telegram_bot/handlers.py
@@ -1,38 +1,49 @@
 import asyncio
+import os
 
-from app.crud.users import add_chat_id_by_login
+from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-from app.telegram_bot.config import TOKEN
+from app.crud.users import add_chat_id_by_login
 from app.database import SessionLocal
+from app.telegram_bot.config import TOKEN
 
 db = SessionLocal()
+application = None
+load_dotenv()
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 
-async def send_message(context):
-    chat_id = context.job.data["chat_id"]
-    await context.bot.send_message(chat_id=chat_id, text="Привет, ты лох!")
 
-
-# Обработчик команды /start
-async def start(update: Update, context):
+# Функция для приветствия при выполнении команды /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
-    print(f"Получен chat_id: {chat_id}")
+    user_name = update.effective_user.first_name  # Имя пользователя в Telegram
 
     if context.args:
-        user_login = context.args[0]
-        add_chat_id_by_login(db, user_login, chat_id)
-        await update.message.reply_text(f"Вы подключены как пользователь с токеном: {user_login}")
+        user_login = context.args[0]  # Логин из ссылки
+        # add_chat_id_by_login(db, user_login, chat_id)  # Сохраняем chat_id
+        await update.message.reply_text(
+            f"Привет, {user_name}! Вы подключены как пользователь с логином {user_login}. Ваш chat_id сохранён."
+        )
     else:
-        await update.message.reply_text("Ошибка: логин отсутствует!")
+        await update.message.reply_text(
+            "Ошибка: логин отсутствует! Убедитесь, что вы перешли по правильной ссылке."
+        )
 
 
-# Основная функция
+# Функция для отправки сообщения по chat_id
+async def send_telegram_message(chat_id: int, message: str):
+    try:
+        await application.bot.send_message(chat_id=chat_id, text=message)
+    except Exception as e:
+        print(f"Ошибка при отправке сообщения: {e}")
+
+
+# Запускаем бота
 def main():
-    # Создаём приложение
-    application = Application.builder().token(TOKEN).build()
-
-    # Добавляем обработчик команды /start
+    global application
+    application = Application.builder().token(TELEGRAM_TOKEN).build()
     application.add_handler(CommandHandler("start", start))
 
     application.run_polling()
